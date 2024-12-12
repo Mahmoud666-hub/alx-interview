a22dfa34a9ca9c1c1be31da183774d34f0597da4
mahmoud <mahmoudmodi75@gmail.com>
2024-12-12 21:27:01 +0200

    ooode

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """
-    Prime Game
+    Prime Games
 """
 
 
 def isWinner(x, nums):
-    """Determine the winner of the Prime Game"""
+    """Determine the winner of the Prime Games"""
     if x <= 0 or not nums:
         return None
     if x != len(nums):
@@ -14,17 +14,17 @@ def isWinner(x, nums):
     ben = 0
     maria = 0
 
-    max_num = max(nums)
-    is_prime = [True] * (max_num + 1)
+    max_numb = max(nums)
+    is_prime = [True] * (max_numb + 1)
     is_prime[0], is_prime[1] = False, False
 
-    for i in range(2, int(max_num**0.5) + 1):
+    for i in range(2, int(max_numb**0.5) + 1):
         if is_prime[i]:
-            for j in range(i * i, max_num + 1, i):
+            for j in range(i * i, max_numb + 1, i):
                 is_prime[j] = False
 
-    prime_count = [0] * (max_num + 1)
-    for i in range(1, max_num + 1):
+    prime_count = [0] * (max_numb + 1)
+    for i in range(1, max_numb + 1):
         prime_count[i] = prime_count[i - 1] + (1 if is_prime[i] else 0)
 
     for n in nums:
